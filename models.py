6908470bd1f67c83ebac89a084ab74496e60fa35
josephkinuthia <128019685+josephkinuthia@users.noreply.github.com>
2023-04-24 01:44:02 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+
+def __str__(self):
+    return self.firstname + ' ' + self.lastname
+
+
